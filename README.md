07c6e933c6738cc6c81cfe3f4de42243370ffbf4
jjryoutubegaming <42552126+jjryoutubegaming@users.noreply.github.com>
2021-07-31 02:01:14 +0700

    up final

---
+++ README.md
@@ -0,0 +1,4 @@
+# termux-f609
+USED FOR RUNNING PHP AND PULL THIS REPO :)
+The android app is coming :)
+Translation maybe kinda late
---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Below This is your variable change for your router config
+ * Library from walangkaji
+ * And remake and recode by Orangeskai
+ * @modder Orangeskai (https://github.com/Orangeskai)
+ * @author walangkaji (https://github.com/walangkaji)
+ */
+
+//your ip address of your router
+$ipModem  = "";
+
+//user and password login router
+$username = "";
+$password = "";
+
+// to enable debug ? *no need ('')
+$debug    = true;
